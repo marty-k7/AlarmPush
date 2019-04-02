9fb29084896912ff3f246abcb27ace71c877f62c
marty.k7 <martynas.klastaitis@gmail.com>
2019-04-02 09:15:40 +0300

    Initial Commit

---
+++ AlarmPush/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlarmPush
+//
+//  Created by Martynas Klastaitis  on 02/04/2019.
+//  Copyright © 2019 bajoraiciuprodukcija. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
